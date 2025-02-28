f46ccc900994e6f76facc0b5864ee9ddf436f4b1
naneunduckfeel <bong2787@gmail.com>
2025-02-28 16:12:22 +0900

    hss introduce page

---
+++ team.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="뮤지컬 하트셉수트 알아보기">
+    <meta name="keywords" content="musical, hatshepsut, ancient egypt, pharaoh, amun, 뮤지컬, 하트셉수트, 이집트, 파라오, 아문">
+    <title>Musical Hatshepsut - Creators</title>
+  </head>
+
+  <body>
+    <header>
+      <h1><a href="../index.php" title="Musical <Hatshepsut>" target="_self">
+        Musical &lt;Hatshepsut&gt;
+      </a></h1>
+    </header>
+
+    <nav>
+      <ol>
+        <li><a href="./intro.php" title="뮤지컬 하트셉수트 소개">Introduce</a></li>
+        <li><a href="./synop.php" title="뮤지컬 하트셉수트 시놉시스">Synopsis</a></li>
+        <li><a href="./team.php" title="뮤지컬 하트셉수트 창작진">Creative Team</a></li>
+        <li><a href="./cast.php" title="뮤지컬 하트셉수트 배역&배우">Character&Cast</a></li>
+      </ol>
+    </nav>
+
+    <article>
+      <h2>Creative Team</h2>
+      <img src="../images/creators.jpg" alt="뮤지컬 하트셉수트 시놉시스" width=70%>
+
+      <ul>
+        <li>프로듀서 이재은</li>
+        <li>작/작사 정필 | 작곡 남지영</li>
+        <li>연출 박지혜 | 음악감독 하태성 | 안무감독 이현정</li>
+        <li>무대디자인 이엄지 | 조명디자인 정구홍 | 음향디자인 한문규</li>
+        <li>의상디자인 홍문기 | 소품디자인 김혜지 | 분장디자인 정지윤</li>
+        <li>무대감독 진정만 | 제작PD 김준섭 | 컴퍼니매니저 조민영</li>
+      </ul>
+      
+      <table border=1>
+        <tr>
+          <td colspan=6 align="center">프로듀서 이재은</td>
+        </tr>
+        <tr>
+          <td colspan=3 align="center">작/작사 정필</td>
+          <td colspan=3 align="center">작곡 남지영</td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">연출 박지혜</td>
+          <td colspan="2" align="center">음악감독 하태성</td>
+          <td colspan="2" align="center">안무감독 이현정</td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">무대디자인 이엄지</td>
+          <td colspan="2" align="center">조명디자인 정구홍</td>
+          <td colspan="2" align="center">음향디자인 한문규</td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">의상디자인 홍문기</td>
+          <td colspan="2" align="center">소품디자인 김혜지</td>
+          <td colspan="2" align="center">분장디자인 정지윤</td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center">무대감독 진정만</td>
+          <td colspan="2" align="center">제작PD 김준섭</td>
+          <td colspan="2" align="center">컴퍼니매니저 조민영</td>
+        </tr>
+      </table>      
+<!-- css style 태그로 손볼것 특히 센터정렬은 그쪽이 더 권장된다함 -->
+    </article>
+  </body>
+</html>sdfdfsdfsdfsdf
